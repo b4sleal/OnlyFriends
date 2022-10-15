481adb287cc800753fcd250e61945c8ac210fb35
Amber Yang <amber.s.yang116@gmail.com>
2022-10-15 19:30:46 -0400

    LogInPage Design

--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
-import React from 'react';
+import { Route, Routes } from "react-router-dom";
+import React from "react";
 
-import { MainPage } from './components/MainPage';
-import { Register } from './components/Register';
-import { LoggedIn } from './components/LoggedIn';
-import { LogInPage } from './components/LogInPage';
+import { MainPage } from "./components/MainPage";
+import { Register } from "./components/Register";
+import { LoggedIn } from "./components/LoggedIn";
+import { LogInPage } from "./components/LogInPage";
 //import { Login } from './components/Login';
 
 export default (
-    <Routes>
-        {/**
-         * 
-         * 
-         * the * means that anything besides the elements below wil just route to the main page
-         * you have to add the route to loginpage below
-         */}
-        <Route path="*" element={<MainPage />} />
-        <Route path="home" element={<MainPage />} />
-        <Route path="register" element={<Register />} />
-        <Route path="loggedin" element={<LoggedIn />} />
-        <Route path="LogInPage" element={<LogInPage />} />
-    </Routes>
+	<Routes>
+		{/**
+		 *
+		 *
+		 * the * means that anything besides the elements below wil just route to the main page
+		 * you have to add the route to loginpage below
+		 */}
+		<Route path="*" element={<MainPage />} />
+		<Route path="home" element={<MainPage />} />
+		<Route path="register" element={<Register />} />
+		<Route path="loggedin" element={<LoggedIn />} />
+		<Route path="LogInPage" element={<LogInPage />} />
+	</Routes>
 );
 
 //        <Route path="login" element={<Login />} />
